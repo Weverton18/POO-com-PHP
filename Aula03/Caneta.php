16987ce746ae2100a0c241c8eb9647a0d199d335
Weverton18 <wevertonardo17@gmail.com>
2022-03-02 20:12:47 -0300

    Visibilidade de um Objeto

---
+++ Aula03/Caneta.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Caneta{
+        public $modelo;
+        public $cor;
+        private $ponta;
+        protected $carga;
+        protected $tampada;
+
+        public function rabiscar(){
+            if($this->tampada){
+                echo "<p>ERRO! Não posso rabiscar!</p>";
+            }else{
+                echo "<p>Estou Rabiscando ...</p>";
+            }
+        }
+
+        public function tampar(){
+            $this->tampada = true;
+        }
+
+        public function destampar(){
+            $this->tampada = false;
+        }
+    }
